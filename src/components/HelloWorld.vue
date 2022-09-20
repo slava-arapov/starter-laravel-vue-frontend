6b015d304be2062d264b0e8f8c2ce6b33b05ed80
Slava Arapov <sla.ckf@gmail.com>
2022-09-20 10:23:01 +0300

    Some text fixes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,12 +2,12 @@
   <v-row align="center">
     <v-col class="mb-4">
       <h1 class="text-h1 font-weight-black mt-3 mb-6 info--text">
-        Laravel + Vue&nbsp;2<br>Starter
+        Laravel + Vue&nbsp;3<br>Starter
       </h1>
       <p class="subheading font-weight-regular">
         Laravel ◦ Sanctum ◦ Fortify ◦ Telescope
         <br>
-        Vue&nbsp;2 ◦ Typescript ◦ Vuetify
+        Vue&nbsp;3 ◦ Typescript ◦ Vuetify
       </p>
       <p v-if="msg">
         {{ msg }}
@@ -18,8 +18,6 @@
 
 <script lang='ts'>
 import { defineComponent } from 'vue';
-
-// Logo
 
 export default defineComponent({
   name: 'HelloWorld',
