217c7db5e59ac8d4f754aa03a687c13aa02ee5bb
Slava Arapov <sla.ckf@gmail.com>
2022-11-07 09:18:56 +0300

    Useless components deleted

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,14 @@
 <template>
-  <hello-world />
+  <v-row align="center">
+    <v-col class="mb-4">
+      <h1 class="text-h1 font-weight-black mt-3 mb-6 info--text">
+        Laravel + Vue&nbsp;3<br>Starter
+      </h1>
+      <p class="subheading font-weight-regular">
+        Laravel ◦ Sanctum ◦ Fortify ◦ Telescope
+        <br>
+        Vue&nbsp;3 ◦ Typescript ◦ Vuetify
+      </p>
+    </v-col>
+  </v-row>
 </template>
-
-<script setup lang="ts">
-import HelloWorld from '../components/HelloWorld.vue';
-
-</script>
